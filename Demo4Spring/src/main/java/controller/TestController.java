2c0b9a1b8e288d04a5b78579f01b2177754d2c7d
xiaoFL <398993327@qq.com>
2018-05-04 17:29:05 +0800

    new change

--- Demo4Spring/src/main/java/controller/TestController.java
+++ Demo4Spring/src/main/java/controller/TestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class TestController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String index() {
         return "index";
     }
